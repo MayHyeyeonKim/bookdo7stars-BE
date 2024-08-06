4c994c5e237c8eddc5b8346ae97f41e1e11b5ca6
MayHyeyeonKim <hkim7963@gmail.com>
2024-08-06 13:56:37 -0700

    changed frontend address

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.use(cors());
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://bookdo-bookstore.netlify.app', credentials: true }));
 app.use(
   session({
     saveUninitialized: false,
